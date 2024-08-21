83e27dc4897d274856d47882213826c6adedc9be
Devonion22 <devonion.20@gmail.com>
2024-08-21 17:33:51 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 4)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-4xl font-bold underline">
+        Error 404
+      </h1>
+      <h1 className="text-4xl font-italic">
+        Page not found
+      </h1>
+      <h1 className="text-4xl font-italic">
+        Contact with me <a href='https://t.me/neverisendthe' className="text-4xl font-bold">Telegram</a>
+      </h1>
     </>
   )
 }
